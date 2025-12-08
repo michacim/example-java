518ff064ebedf407478d4ef1756e0831eaf42cdb
Micha Schirmer <michajava8@gmail.com>
2025-12-08 12:11:56 +0100

    tab include - next Tab

--- tab_include/src/main/java/com/example/tab_include/HelloController.java
+++ tab_include/src/main/java/com/example/tab_include/HelloController.java
@@ -1,5 +1,6 @@
 package com.example.tab_include;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TabPane;
 
@@ -7,5 +8,14 @@ public class HelloController {
 
     @FXML
     private TabPane tabPane;
+
+    public void onNext(ActionEvent actionEvent) {
+        tabPane.getSelectionModel().selectNext();
+    }
+    
+    public void onBack(ActionEvent actionEvent) {
+        tabPane.getSelectionModel().selectPrevious();
+    }
+
 
 }
